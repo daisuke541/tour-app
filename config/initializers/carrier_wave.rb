efa217b4b2542ae2c01fbece5823880d61d954e5
daisuke541 <sirius0509@gmail.com>
2020-07-02 07:55:00 +0000

    update

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,14 @@ if Rails.env.production?
 CarrierWave.configure do |config|
 config.fog_credentials = {
   :provider              => 'AWS',
-  :region                => ENV['S3_REGION'],
-  :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-  :aws_secret_access_key => ENV['S3_SECRET_KEY']
+  :region                => 'ap-northeast-1',
+  :aws_access_key_id     => ENV['S3_ACCESS_KEY_ID'],
+  :aws_secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
 }
 config.fog_provider = 'fog/aws'
-config.fog_directory = ENV['S3_BUCKET']
+config.fog_directory = 'tour-appdaisuke'
+config.asset_host = 'https://s3.amazonaws.com/tour-appdaisuke'
+config.fog_public = false
+config.storage :fog
 end
-end
+end 
