61f689a0c657eaa73c19c297ddcc56c08ac0ee5c
daisuke541 <sirius0509@gmail.com>
2020-07-30 08:08:19 +0000

    Rspec追加

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,21 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 
-module ToyApp
+module Tourapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.assets.initialize_on_precompile = false
+    
+    config.generators do |g|
+      g.test_framework :rspec
+                      helper_specs: false,
+                      routing_specs: false,
+                      view_specs: false,
+                      controller_specs: false
+    end 
+    config.generators.system_tests = nil
   end
 end
