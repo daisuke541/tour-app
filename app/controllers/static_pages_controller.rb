4f6795aa68cfbc003c380bf75efd26cb6f16c81b
daisuke541 <sirius0509@gmail.com>
2020-09-19 09:18:36 +0000

    タグ検索機能追加途中

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build 
+      @posts = params[:tag_id].present? ? Tag.find(params[:tag_id]).posts: Posts.all
       @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
     end 
   end
